7295e9ba06cfefb9fd6bbfa4f9c4d769d9a9cf55
Maksym K <maxkr1203@gmail.com>
2025-04-19 15:29:28 +0300

    feat(19-20): tickets and payemtns

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -3,8 +3,10 @@ import { Module } from '@nestjs/common';
 import { CommentModule } from './comments/comment.module';
 import { CompanyModule } from './company/company.module';
 import { EventModule } from './event/event.module';
+import { PaymentsModule } from './payments/payments.module';
 import { ReactionModule } from './reactions/reaction.module';
 import { StripeModule } from './stripe/stripe.module';
+import { TicketsModule } from './tickets/tickets.module';
 
 @Module({
   imports: [
@@ -13,6 +15,8 @@ import { StripeModule } from './stripe/stripe.module';
     StripeModule,
     CommentModule,
     ReactionModule,
+    TicketsModule,
+    PaymentsModule,
   ],
 })
 export class ModulesModule {}
